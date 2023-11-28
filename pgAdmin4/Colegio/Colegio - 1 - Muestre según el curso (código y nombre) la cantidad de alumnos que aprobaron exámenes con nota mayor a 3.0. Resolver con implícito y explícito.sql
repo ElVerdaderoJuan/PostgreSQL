f4a4396eff01ee8b210ed4f6954838554b3cbf45
Juan <70086298+ElVerdaderoJuan@users.noreply.github.com>
2023-11-27 19:11:25 -0500

    Create Colegio - 1 - Muestre según el curso (código y nombre) la cantidad de alumnos que aprobaron exámenes con nota mayor a 3.0. Resolver con implícito y explícito.sql

---
+++ pgAdmin4/Colegio/Colegio - 1 - Muestre según el curso (código y nombre) la cantidad de alumnos que aprobaron exámenes con nota mayor a 3.0. Resolver con implícito y explícito.sql
@@ -0,0 +1,21 @@
+-- Implícito
+-- Muestre según el curso (código y nombre) la cantidad de alumnos que aprobaron exámenes con nota mayor a 3.0
+SELECT cu.cod_cur, cu.nom_cur, COUNT(cu.cod_cur) AS Cantidad_alumnos
+FROM aplicacion_examen a_e, estudiante es, curso cu
+WHERE a_e.fk_ide_alu = es.ide_alu
+AND es.fk_cod_cur = cu.cod_cur
+AND nta_apl > 3.0
+GROUP BY cu.cod_cur, cu.nom_cur
+ORDER BY cantidad_alumnos
+
+
+
+-- Explícito
+-- Muestre según el curso (código y nombre) la cantidad de alumnos que aprobaron exámenes con nota mayor a 3.0
+SELECT cu.cod_cur, cu.nom_cur, COUNT(cu.cod_cur) AS Cantidad_alumnos
+FROM curso cu
+JOIN estudiante es ON es.fk_cod_cur = cu.cod_cur
+JOIN aplicacion_examen a_e ON a_e.fk_ide_alu = es.ide_alu
+WHERE a_e.nta_apl > 3.0
+GROUP BY cu.cod_cur, cu.nom_cur
+ORDER BY cantidad_alumnos
